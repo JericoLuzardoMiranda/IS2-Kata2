e8f868a1d4a7e756488b7b4c1b7d0504eb63b70b
JericoLuzardoMiranda <125827592+JericoLuzardoMiranda@users.noreply.github.com>
2023-11-01 13:17:37 +0000

    The calculation has been made for statistics on organizations based on their country of origin.

--- src/software/ulpgc/kata2/FoundedOrganizationStatistic.java
+++ src/software/ulpgc/kata2/FoundedOrganizationStatistic.java
@@ -1,4 +1,19 @@
 package software.ulpgc.kata2;
 
-public class FoundedOrganizationStatistic {
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class FoundedOrganizationStatistic implements OrganizationStatistic {
+
+    @Override
+    public Map<String, Integer> calculate(List<Organization> organizations) {
+        Map<String, Integer> result = new HashMap<>();
+        for (Organization organization : organizations) {
+            String country = organization.getCountry();
+            result.put(country, result.getOrDefault(country, 0) + 1);
+        }
+        return result;
+    }
+
 }
