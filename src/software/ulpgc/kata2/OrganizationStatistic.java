f9875a2734616a27ea1b51341832b362773b2d16
JericoLuzardoMiranda <125827592+JericoLuzardoMiranda@users.noreply.github.com>
2023-11-01 13:15:32 +0000

    Added FoundedOrganizationStatistic class.

--- src/software/ulpgc/kata2/OrganizationStatistic.java
+++ src/software/ulpgc/kata2/OrganizationStatistic.java
@@ -1,4 +1,9 @@
 package software.ulpgc.kata2;
 
+import java.util.List;
+import java.util.Map;
+
 public interface OrganizationStatistic {
+    Map<String, Integer> calculate(List<Organization> organizations);
 }
+
